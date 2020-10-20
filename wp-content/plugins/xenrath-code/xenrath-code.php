ed5e2df13cd22537bb9b700f0d6117bf1ae4d407
xenrath <xenrath89@gmail.com>
2020-10-20 21:33:09 +0700

    Get Data from Database pada Pembuatan Plugin WordPress (4)

--- wp-content/plugins/xenrath-code/xenrath-code.php
+++ wp-content/plugins/xenrath-code/xenrath-code.php
@@ -27,3 +27,43 @@ function xenrath_setting_page(){
 	<p>Developed by xenrath.co.id</p>
 	<?php
 }
+
+// Get Data Form Database 
+function get_data_users(){
+	global $wpdb;
+	$result = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}users", OBJECT);
+	return $result;
+}
+
+add_shortcode('xenrath_users', 'table_users');
+
+function table_users(){
+	ob_start();
+	?>
+
+	<table>
+		<thead>
+			<tr>
+				<th>No. </th>
+				<th>Username</th>
+				<th>Email</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+				$no = 1;
+				foreach (get_data_users() as $row) {
+					echo "
+						<tr>
+							<td>".$no++.".</td>
+							<td>".$row->user_login."</td>
+							<td>".$row->user_email."</td>
+						</tr>";
+				}
+			 ?>
+		</tbody>
+	</table>
+
+	<?php 
+	return ob_get_clean();
+}
